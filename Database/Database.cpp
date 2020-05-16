c1aba4ffabed4c10c06ca64876752783eada928d
Akshat Harit <akshat.harit@gmail.com>
2020-05-16 13:30:35 -0700

    Part 1. A simple repl

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -3,18 +3,28 @@
 
 #include <iostream>
 
-int main()
+
+void print_prompt() 
 {
-    std::cout << "Hello World!\n";
+    std::cout << "db > ";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+std::string get_input() {
+    std::string s;
+    std::cin >> s;
+    return s;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main(int argc, char* argv[])
+{
+    while (true) {
+        print_prompt();
+        auto input = get_input();
+        if (input == ".exit") {
+            return EXIT_SUCCESS;
+        }
+        else {
+            std::cout << "Unrecognized command: " << input << "\n";
+        }
+    }
+}
